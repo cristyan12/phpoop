1eeff9caa65a58eb2691c4c5df40aa5f0cd404ea
Cristyan Valera <numenor21@gmail.com>
2020-10-20 17:14:14 -0400

    wip

---
+++ polymorphism.php
@@ -0,0 +1,126 @@
+<?php
+declare(strict_types=1);
+
+function show(string $message): void {
+    echo "<p>$message</p>";
+}
+
+abstract class Unit
+{
+    protected float $hp = 40;
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getHp(): float
+    {
+        return $this->hp;
+    }
+
+    public function move(string $direction): void
+    {
+        show("{$this->name} camina hacia $direction");
+    }
+
+    abstract public function attack(Unit $opponent): void;
+
+    public function takeDamage(float $damage): void
+    {
+        $this->hp = $this->hp - $this->absorbDamage($damage);
+
+        show("{$this->name} => {$this->hp} puntos de vida.");
+
+        if ($this->getHp() <= 0) {
+            $this->die();
+        }
+    }
+
+    private function die(): void
+    {
+        show("{$this->name} muere");
+
+        exit();
+    }
+
+    protected function absorbDamage(float $damage): float
+    {
+        return $damage;
+    }
+}
+
+interface Armor
+{
+    public function absorbDamage(float $damage): float;
+}
+
+class BronzeArmor implements Armor
+{
+    public function absorbDamage(float $damage): float
+    {
+        return $damage / 2;
+    }
+}
+
+class Soldier extends Unit
+{
+    protected int $damage = 40;
+    protected ?Armor $armor = null;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+    }
+
+    public function setArmor(?Armor $armor = null): void
+    {
+        $this->armor = $armor;
+    }
+
+    public function attack(Unit $opponent): void
+    {
+        show("{$this->name} ataca con la espada a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function absorbDamage(float $damage): float
+    {
+        if ($this->armor) {
+            $damage = $this->armor->absorbDamage($damage);
+        }
+
+        return $damage;
+    }
+}
+
+class Archer extends Unit
+{
+    protected int $damage = 30;
+
+    public function attack(Unit $opponent): void
+    {
+        show("{$this->name} dispara una flecha a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+}
+
+$armor = new BronzeArmor();
+
+$soldier = new Soldier('Ramm');
+$archer = new Archer('Silence');
+
+$archer->attack($soldier);
+
+$soldier->attack($archer);
+$soldier->setArmor($armor);
+
+$archer->attack($soldier);
