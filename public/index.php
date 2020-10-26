e3e020cdb1028f038d903aed0776e14f4c52270c
Cristyan Valera <numenor21@gmail.com>
2020-10-25 23:21:21 -0400

    magic methods __get, __set, __isset, __unset

--- public/index.php
+++ public/index.php
@@ -4,19 +4,17 @@ namespace Beleriand;
 
 require '../vendor/autoload.php';
 
-$user = new User([
+$user = new User;
+
+$user->fill([
     'first_name' => 'Cristyan',
     'last_name' => 'Valera',
 ]);
 
 $user->nickname = 'cristyan12';
 
-echo "<pre>";
-
-var_dump($user);
-
-exit();
-
-echo "<p>Nickname: {$user->nickname}</p>";
-
 echo "<p>Bienvenido, {$user->first_name} {$user->last_name}</p>";
+
+if (isset($user->nickname)) {
+    echo "<p>Nickname: {$user->nickname}</p>";
+}
--- src/User.php
+++ src/User.php
@@ -8,7 +8,22 @@ class User
 
     public function __construct(array $attributes = [])
     {
+        $this->fill($attributes);
+    }
+
+    public function fill(array $attributes = []): void
+    {
         $this->attributes = $attributes;
+    }
+
+    public function setAttribute(string $name, string $value): void
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    public function getAttributes(): array
+    {
+        return $this->attributes;
     }
 
     public function getAttribute(string $name): string
@@ -20,8 +35,28 @@ class User
         return 'N/D';
     }
 
+    public function hasAttribute(string $name): bool
+    {
+        return isset($this->attributes[$name]);
+    }
+
+    public function __set(string $name, string $value): void
+    {
+        $this->setAttribute($name, $value);
+    }
+
     public function __get(string $name): string
     {
         return $this->getAttribute($name);
+    }
+
+    public function __isset(string $name): bool
+    {
+        return $this->hasAttribute($name);
+    }
+
+    public function __unset(string $name): void
+    {
+        unset($this->attributes[$name]);
     }
 }
