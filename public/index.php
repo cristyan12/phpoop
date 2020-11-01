197ce8a906d66b917482fdeb3c91697c15c33a15
Cristyan Valera <numenor21@gmail.com>
2020-10-31 23:20:12 -0400

    lesson26 project dependencies

--- public/index.php
+++ public/index.php
@@ -3,27 +3,11 @@ declare(strict_types=1);
 
 namespace Beleriand;
 
-// require '../vendor/Laravel/Macroable.php';
-// require '../vendor/Laravel/HtmlBuilder.php';
 require '../vendor/autoload.php';
 
-use Laravel\HtmlBuilder;
+$user = new User([
+    'name' => 'Walter White',
+    'birthDate' => '07/09/1959',
+]);
 
-try {
-    HtmlBuilder::macro('success', function(string $message): string {
-        return "<p style=\"background-color: #dff0d8;
-            border-color: #d0e9c6; color: #3c763d;
-            padding: 10px\">{$message}</p>" . $this->hr();
-    });
-
-    HtmlBuilder::macro('p', fn(string $text): string => "<p>{$text}</p>");
-
-    $html = new HtmlBuilder();
-
-    echo $html->success("Todo salió bien!");
-
-    echo $html->p('Esta es una prueba de un párrafo.');
-
-} catch (\BadMethodCallException $e) {
-    echo "BadMethodCallException: {$e->getMessage()}";
-}
+echo "{$user->name} tiene {$user->age} años";
--- src/User.php
+++ src/User.php
@@ -3,51 +3,14 @@ declare(strict_types=1);
 
 namespace Beleriand;
 
+use Carbon\Carbon;
+
 class User extends Model
 {
-    protected LunchBox $lunch;
-
-    public function __construct(array $attributes = [])
+    public function getAgeAttribute(): string
     {
-        parent::__construct($attributes);
+        $date = Carbon::createFromFormat('d/m/Y', $this->birthDate);
 
-        $this->lunch = new LunchBox();  // Null Objects
-    }
-
-    public function setLunch(LunchBox $lunch): void
-    {
-        $this->lunch = $lunch;
-    }
-
-    public function eat(): void
-    {
-        if ($this->lunch->isEmpty()) {
-            throw new \Exception("{$this->name} no tiene nada para comer :(");
-        }
-
-        Str::info("{$this->name} alumerza {$this->lunch->shift()}");
-    }
-
-    public function eatMeal(): void
-    {
-        $food = $this->lunch->filter(fn($food) => ! $food->beverage);
-
-        $beverage = $this->lunch->filter(fn($food) => $food->beverage);
-
-        Str::info(
-            "<b>{$this->name} tiene {$this->lunch->count()} alimentos</b>"
-        );
-
-        Str::info("<b>Alimentos: {$food->count()}</b>");
-
-        foreach ($food as $item) {
-            Str::info(" - {$item->name}");
-        }
-
-        Str::info("<b>Bebidas: {$beverage->count()}</b>");
-
-        foreach ($beverage as $item) {
-            Str::info(" - {$item->name}");
-        }
+        return (string) $date->age;
     }
 }
