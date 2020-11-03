d144eb3e88d3042511196050d29291c983e5b57d
Cristyan Valera <numenor21@gmail.com>
2020-11-03 19:12:22 -0400

    lesson29 array access #2

--- tests/ArrayAccessTest.php
+++ tests/ArrayAccessTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class ArrayAccessTest extends TestCase
 {
     /** @test */
-    function a_model_has_array_access()
+    function it_can_get_offset_with_array_access()
     {
         $user = new UserModel([
             'name' => 'Cristyan Valera',
@@ -20,27 +20,60 @@ class ArrayAccessTest extends TestCase
         $this->assertSame('cristyan12@mail.com', $user['email']);
         $this->assertSame('cristyan12.github.io', $user['website']);
     }
+    /** @test */
+    function it_can_check_if_offset_exists()
+    {
+        $user = new UserModel([
+            'name' => 'Cristyan Valera',
+        ]);
+
+        $this->assertTrue(isset($user['name']));
+        $this->assertFalse(empty($user['name']));
+
+        $this->assertFalse(isset($user['email']));
+        $this->assertTrue(empty($user['email']));
+    }
+    /** @test */
+    function it_set_and_get_values_with_array_access()
+    {
+        $user = new UserModel;
+        $user['name'] = 'Cristyan Valera';
+
+        $this->assertSame('Cristyan Valera', $user['name']);
+    }
+    /** @test */
+    function it_can_set_and_unset_with_array_access()
+    {
+        $user = new UserModel;
+        $user['name'] = 'Cristyan Valera';
+
+        $this->assertTrue(isset($user['name']));
+
+        unset($user['name']);
+
+        $this->assertFalse(isset($user['name']));
+    }
 }
 
 class UserModel extends Model implements \ArrayAccess
 {
     public function offsetExists($offset): bool
     {
-
+        return isset($this->$offset);
     }
 
     public function offsetGet($offset)
     {
-        return $this->getAttribute($offset);
+        return $this->$offset;
     }
 
     public function offsetSet($offset, $value): void
     {
-
+        $this->$offset = $value;
     }
 
     public function offsetUnset($offset): void
     {
-
+        unset($this->$offset);
     }
 }
